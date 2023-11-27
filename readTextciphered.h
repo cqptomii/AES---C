5b3dad0cf754e83701705dfbe4c75a9e10550d69
Cqptomii <tomfraisse.pro@gmail.com>
2023-11-27 19:42:52 +0100

    Add readciphered Text functions

---
+++ readTextciphered.h
@@ -0,0 +1,10 @@
+//
+// Created by tomfr on 27/11/2023.
+//
+
+#ifndef AES_READTEXTCIPHERED_H
+#define AES_READTEXTCIPHERED_H
+#include "keyGenerate.h"
+FILE *selectFile();
+bool readInFile(FILE *file);
+#endif //AES_READTEXTCIPHERED_H
